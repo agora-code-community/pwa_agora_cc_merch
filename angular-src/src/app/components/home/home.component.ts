12110d0cc9d0a9bf3d8efabeb142e0d2479594cf
Mukuma Musenge <kuma.musenge@gmail.com>
2017-10-18 17:59:57 +0200

    Added Carousal to Home Component
    - Built carousel to suit mockup

--- angular-src/src/app/components/home/home.component.ts
+++ angular-src/src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { NgxCarousel } from 'ngx-carousel';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +10,43 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
+
+  public carouselTileItems: Array<any>;
+  public carouselTile: NgxCarousel;
+
+
   constructor(
     private authService: AuthService,
     private router: Router
   ) { }
 
+
   ngOnInit() {
+    this.carouselTileItems = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+    
+       this.carouselTile = {
+         grid: {xs: 2, sm: 3, md: 3, lg: 5, all: 0},
+         slide: 2,
+         speed: 400,
+         animation: 'lazy',
+         point: {
+           visible: true
+         },
+         load: 2,
+         touch: true,
+         easing: 'ease'
+       }
+  }
+
+  public carouselTileLoad(evt: any) {
+    
+    const len = this.carouselTileItems.length
+    if (len <= 10) {
+      for (let i = len; i < len + 1; i++) {
+        this.carouselTileItems.push(i);
+      }
+    }
+
   }
 
 }
