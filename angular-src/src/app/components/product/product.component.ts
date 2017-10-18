194f5818daf57a83d6a843c5bd1a037cb23465ae
Mukuma Musenge <kuma.musenge@gmail.com>
2017-10-18 19:40:16 +0200

    Comments

--- angular-src/src/app/components/product/product.component.ts
+++ angular-src/src/app/components/product/product.component.ts
@@ -8,13 +8,14 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductComponent implements OnInit {
 
+  //Create  a variable to hold the parameter
   type: String;
   private sub: any;
   
     constructor(private route: ActivatedRoute) {}
   
     ngOnInit() {
-
+      //Initialise the component with parameters received from the route
      this.sub = this.route.params.subscribe(params => {
         this.sub = +params['apparel'];
         console.log(this.sub);
