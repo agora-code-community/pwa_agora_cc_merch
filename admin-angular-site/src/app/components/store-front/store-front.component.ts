75287538f62d959ebda51530c692b01f11c7e001
Boroma <boromamwale@gmail.com>
2017-11-01 07:38:37 +0200

    added some assets removed php files

---
+++ admin-angular-site/src/app/components/store-front/store-front.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-store-front',
+  templateUrl: './store-front.component.html',
+  styleUrls: ['./store-front.component.css']
+})
+export class StoreFrontComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
